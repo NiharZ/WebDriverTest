83547165d18f6756b27662a00d1ca5295b34be09
v-nisark <v-nisark@172.30.64.51>
2019-09-13 17:42:32 +0530

    testing change

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -13,7 +13,8 @@ public class NewTest {
 		public void testEasy() {	
 			driver.get("http://demo.guru99.com/test/guru99home/");  
 			String title = driver.getTitle();				 
-			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 
+			//TestComment
 		}	
 		@BeforeTest
 		public void beforeTest() {	
